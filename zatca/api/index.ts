34767d17e2e177410e8fb3afc88d3991efb4a8f6
Sajool IT <133037807+sajoolit@users.noreply.github.com>
2025-12-25 20:34:20 +0300

    Swap SANDBOX_BASEURL values in settings

--- zatca/api/index.ts
+++ zatca/api/index.ts
@@ -4,8 +4,8 @@ import { XMLDocument } from "../../parser";
 import util from "util";
 const settings = {
   API_VERSION: "V2",
-  //SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/developer-portal",
-  SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/simulation",
+  SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/developer-portal",
+  //SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/simulation",
   // SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/core",
 };
 
